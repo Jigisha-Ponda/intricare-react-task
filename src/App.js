e54f0a19069bb7d871ed5cdfd34753fcf465b124
Jigisha Ponda <jigishaadatiya21@gmail.com>
2025-12-11 16:03:31 +0530

    added tasks floating label

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from "react-router-dom";
 import ProductDashboard from "./components/ProductDashboard";
-import FigmaDesign from "./components/FigmaDesign";
 
 function App() {
   return (
     <div className="=w-screen">
-      <ProductDashboard />
-      {/* <Router>
+      <Router>
         <div className="min-h-screen">
           <div className="fixed top-1/2 left-0 p-4 border transform -translate-y-1/2 z-50 flex flex-col gap-3 bg-white">
             <NavLink
@@ -21,7 +19,7 @@ function App() {
               Task 1
             </NavLink>
             <NavLink
-              to="/figma"
+              to="https://intricare-react-task2.vercel.app/"
               className={({ isActive }) =>
                 `px-4 py-2 rounded-lg font-semibold transition-colors duration-300 ${isActive ? "bg-green-500 text-white shadow-md" : "text-green-500 hover:bg-green-100"
                 }`
@@ -33,11 +31,11 @@ function App() {
 
             <Routes>
               <Route path="/" element={<ProductDashboard />} />
-              <Route path="/figma" element={<FigmaDesign />} />
+              <Route path="https://intricare-react-task2.vercel.app/" />
               <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </div>
-      </Router> */}
+      </Router>
     </div>
   );
 }
